23c505db1f62cb7474918e064748419849bea734
Nate Bietz <nbietz+github@gmail.com>
2025-01-22 01:02:11 -0800

    feat: add features from previous version
    - Added `uuid` package and its TypeScript definitions to manage unique identifiers.
    - Updated avatar constants with new entries for diverse character representation.
    - Introduced a new `VOICES` constant to support multiple voice options for avatars.
    - Enhanced `InteractiveAvatar` component to include voice selection and quality options.
    - Implemented chat functionality with ChatGPT, Langflow, and Flowise integration for improved user interaction.

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,15 +1,59 @@
 export const AVATARS = [
   {
-    avatar_id: "Eric_public_pro2_20230608",
-    name: "Edward in Blue Shirt",
+    avatar_id: "default",
+    name: "Monica in Sleeveless",
+  },
+  {
+    avatar_id: "Angela-inblackskirt-20220820",
+    name: "Angela in Black Dress",
+  },
+  {
+    avatar_id: "Anna_public_20240108",
+    name: "Anna in White T-shirt",
+  },
+  {
+    avatar_id: "Anna_public_3_20240108",
+    name: "Anna in Brown T-shirt",
+  },
+  {
+    avatar_id: "Briana_public_3_20240110",
+    name: "Briana in Brown suit",
+  },
+  {
+    avatar_id: "Justin_public_3_20240308",
+    name: "Justin in White Shirt",
+  },
+  {
+    avatar_id: "Kristin_public_2_20240108",
+    name: "Kristin in Black Suit",
+  },
+  {
+    avatar_id: "Kayla-incasualsuit-20220818",
+    name: "Kayla in Casual Suit",
+  },
+  {
+    avatar_id: "Lily_public_pro1_20230614",
+    name: "Leah in Black Suit",
+  },
+    {
+    avatar_id: "Tyler-inshirt-20220721",
+    name: "Tyler in Shirt",
+  },
+  {
+    avatar_id: "Tyler-insuit-20220721",
+    name: "Tyler in Suit",
   },
   {
     avatar_id: "Tyler-incasualsuit-20220721",
     name: "Tyler in Casual Suit",
   },
   {
-    avatar_id: "Anna_public_3_20240108",
-    name: "Anna in Brown T-shirt",
+    avatar_id: "Wade_public_2_20240228",
+    name: "Wade in Black Jacket",
+  },
+  {
+    avatar_id: "Eric_public_pro2_20230608",
+    name: "Edward in Blue Shirt",
   },
   {
     avatar_id: "Susan_public_2_20240328",
@@ -18,6 +62,187 @@ export const AVATARS = [
   {
     avatar_id: "josh_lite3_20230714",
     name: "Joshua Heygen CEO",
+  },
+  {
+    avatar_id: "Wayne_20240711",
+    name: "Wayne",
+  },
+  {
+    avatar_id: "josh_lite3_20230714",
+    name: "Joshua HeyGen CEO",
+  },
+];
+
+export const VOICES = [
+  {
+    voice_id: "c29568d0e4a54715bb62bb40daa67875",
+    language: "English",
+    gender: "Female",
+    name: "Alison - Cheerful",
+    preview_audio:
+      "",
+    support_pause: false,
+    emotion_support: false,
+  },
+  {
+    voice_id: "71b0aa6499f6458e8b040818a017db1f",
+    language: "English",
+    genger: "Male",
+    name: "Nova",
+    preview_audio:
+      "",
+    support_pause: false,
+    emotion_support: false,
+  },
+  {
+    voice_id: "8273e0a033074b5bb98d7ce3ab727bd9",
+    language: "English",
+    gender: "Male",
+    name: "Shimmer",
+    preview_audio:
+      "",
+    support_pause: false,
+    emotion_support: false,
+  },
+  {
+    voice_id: "bfc6d0242de24106a104339f0618b68d",
+    language: "English",
+    gender: "Male",
+    name: "Alloy",
+    preview_audio:
+      "",
+    support_pause: false,
+    emotion_support: false,
+  },
+  {
+    voice_id: "6d091fbb994c439eb9d249ba8b0e62da",
+    language: "English",
+    gender: "Male",
+    name: "Jahmai - Friendly",
+    preview_audio:
+      "",
+    support_pause: false,
+    emotion_support: false,
+  },
+  {
+    voice_id: "8b92884579014f8e8147836bbd0c13ca",
+    language: "English",
+    gender: "Male",
+    name: "Fable",
+    preview_audio:
+      "",
+    support_pause: false,
+    emotion_support: false,
+  },
+  {
+    voice_id: "077ab11b14f04ce0b49b5f6e5cc20979",
+    language: "English",
+    gender: "Male",
+    name: "Paul - Natural",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/k6dKrFe85PisZ3FMLeppUM.mp3",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "131a436c47064f708210df6628ef8f32",
+    language: "English",
+    gender: "Female",
+    name: "Amber - Friendly",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/5HHGT48B6g6aSg2buYcBvw.wav",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "0ebe70d83b2349529e56492c002c9572",
+    language: "English",
+    gender: "Male",
+    name: "Antoni - Friendly",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/TwupgZ2az5RiTnmAifPmmS.mp3",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "1bd001e7e50f421d891986aad5158bc8",
+    language: "English",
+    gender: "Female",
+    name: "Sara - Cheerful",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/func8CFnfVLKF2VzGDCDCR.wav",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "001cc6d54eae4ca2b5fb16ca8e8eb9bb",
+    language: "Spanish",
+    gender: "Male",
+    name: "Elias - Natural",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/JmCb3rgMZnCjCAA9aacnGj.wav",
+    support_pause: false,
+    emotion_support: false,
+  },
+  {
+    voice_id: "00988b7d451d0722635ff7b2b9540a7b",
+    language: "Portuguese",
+    gender: "Female",
+    name: "Brenda - Professional",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/fec6396adb73461c9997b2c0d7759b7b.wav",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "00c8fd447ad7480ab1785825978a2215",
+    language: "Chinese",
+    gender: "Female",
+    name: "Xiaoxuan - Serious",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/909633f8d34e408a9aaa4e1b60586865.wav",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "00ed77fac8b84ffcb2ab52739b9dccd3",
+    language: "Latvian",
+    gender: "Male",
+    name: "Nils - Affinity",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/KwTwAz3R4aBFN69fEYQFdX.wav",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "02bec3b4cb514722a84e4e18d596fddf",
+    language: "Arabic",
+    gender: "Female",
+    name: "Fatima - Professional",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/930a245487fe42158c810ac76b8ddbab.wav",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "04e95f5bcb8b4620a2c4ef45b8a4481a",
+    language: "Ukrainian",
+    gender: "Female",
+    name: "Polina - Professional",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/ntekV94yFpvv4RgBVPqW7c.wav",
+    support_pause: true,
+    emotion_support: false,
+  },
+  {
+    voice_id: "071d6bea6a7f455b82b6364dab9104a2",
+    language: "German",
+    gender: "Male",
+    name: "Jan - Natural",
+    preview_audio:
+      "https://static.heygen.ai/voice_preview/fa3728bed81a4d11b8ccef10506af5f4.wav",
+    support_pause: true,
+    emotion_support: false,
   },
 ];
 
--- components/InteractiveAvatar.tsx
+++ components/InteractiveAvatar.tsx
@@ -18,21 +18,49 @@ import {
   Tabs,
   Tab,
 } from "@nextui-org/react";
+import { useChat } from "ai/react";
+import OpenAI from "openai";
+import LangflowClient from "@/app/lib/LangflowClient";
+import FlowiseClient from "@/app/lib/FlowiseClient";
 import { useEffect, useRef, useState } from "react";
 import { useMemoizedFn, usePrevious } from "ahooks";
 
 import InteractiveAvatarTextInput from "./InteractiveAvatarTextInput";
 
-import {AVATARS, STT_LANGUAGE_LIST} from "@/app/lib/constants";
+import {AVATARS, VOICES, STT_LANGUAGE_LIST} from "@/app/lib/constants";
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+// Instantiate a new LangflowClient
+const LANGFLOW_API_KEY = process.env.NEXT_PUBLIC_LANGFLOW_API_KEY ?? "";
+const LANGFLOW_BASE_URL = process.env.NEXT_PUBLIC_LANGFLOW_BASE_URL ?? "";
+const langflowClient = new LangflowClient(
+  LANGFLOW_BASE_URL,
+  LANGFLOW_API_KEY
+);
+
+// Instantiate a new FlowiseClient
+const FLOWISE_API_KEY = process.env.NEXT_PUBLIC_FLOWISE_API_KEY ?? "";
+const FLOWISE_BASE_URL = process.env.NEXT_PUBLIC_FLOWISE_BASE_URL ?? "";
+const flowiseClient = new FlowiseClient(
+  FLOWISE_BASE_URL,
+  FLOWISE_API_KEY
+);
 
 export default function InteractiveAvatar() {
+  const [chatSessionId, setChatSessionId] = useState<string | null>(null);
   const [isLoadingSession, setIsLoadingSession] = useState(false);
   const [isLoadingRepeat, setIsLoadingRepeat] = useState(false);
+  const [isLoadingChat, setIsLoadingChat] = useState(false);
   const [stream, setStream] = useState<MediaStream>();
   const [debug, setDebug] = useState<string>();
   const [knowledgeId, setKnowledgeId] = useState<string>("");
   const [avatarId, setAvatarId] = useState<string>("");
   const [language, setLanguage] = useState<string>('en');
+  const [voiceId, setVoiceId] = useState<string>("");
+  const [quality, setQuality] = useState<AvatarQuality>(AvatarQuality.Medium);
 
   const [data, setData] = useState<StartAvatarResponse>();
   const [text, setText] = useState<string>("");
@@ -40,6 +68,35 @@ export default function InteractiveAvatar() {
   const avatar = useRef<StreamingAvatar | null>(null);
   const [chatMode, setChatMode] = useState("text_mode");
   const [isUserTalking, setIsUserTalking] = useState(false);
+
+  const { input, setInput, handleSubmit } = useChat({
+    onFinish: async (message) => {
+      console.log("ChatGPT Response:", message);
+
+      if (!avatar.current) {
+        setDebug("Avatar API not initialized");
+        return;
+      }
+
+      //send the ChatGPT response to the Interactive Avatar
+      await avatar.current
+        .speak({
+          text: message.content, taskType: TaskType.REPEAT, taskMode: TaskMode.SYNC
+        })
+        .catch((e) => {
+          setDebug(e.message);
+        });
+      setIsLoadingChat(false);
+    },
+    initialMessages: [
+      {
+        id: "1",
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+    ],
+  });
+
 
   async function fetchAccessToken() {
     try {
@@ -176,6 +233,20 @@ export default function InteractiveAvatar() {
   }, []);
 
   useEffect(() => {
+    const fetchSessionId = async () => {
+      // Get the session ID from the session API endpoint
+      const response = await fetch("/api/session", {
+        method: "GET",
+      });
+      const sessionCookie = await response.text(); // Declare and assign the value of sessionCookie
+      setChatSessionId(sessionCookie);
+      console.log("SessionID: ", sessionCookie);
+    };
+  
+    fetchSessionId();
+  }, []);
+
+  useEffect(() => {
     if (stream && mediaStream.current) {
       mediaStream.current.srcObject = stream;
       mediaStream.current.onloadedmetadata = () => {
@@ -184,11 +255,112 @@ export default function InteractiveAvatar() {
       };
     }
   }, [mediaStream, stream]);
+  async function handleSubmitToLangflow() {
+
+    const flowIdOrName = process.env.NEXT_PUBLIC_LANGFLOW_ENDPOINT ?? "";
+    const langflowId = process.env.NEXT_PUBLIC_LANGFLOW_FLOW_ID ?? "";
+    const inputValue = input;
+    const stream = false;
+    const tweaks = {
+      "ChatInput-AEH7b": {},
+      "AstraVectorStoreComponent-KGShY": {},
+      "ParseData-KThnt": {},
+      "Prompt-NJmAO": {},
+      "ChatOutput-YV57G": {},
+      "AstraVectorStoreComponent-seBT4": {},
+      "OpenAIEmbeddings-eS4p1": {},
+      "OpenAIEmbeddings-ZpgE1": {},
+      "OpenAIModel-u4eQs": {},
+      "FirecrawlCrawlApi-8rreb": {},
+      "FirecrawlScrapeApi-qW6sy": {},
+      "SplitText-VNK9A": {},
+      "AstraVectorize-eBOo3": {},
+      "AstraVectorize-qdTfB": {},
+      "ParseData-RT6hq": {},
+      "FilterData-rQcI1": {},
+      "RecursiveCharacterTextSplitter-hg0BJ": {},
+      "CreateData-2ESH7": {},
+      "File-WsZ45": {},
+      "URL-WgEGh": {},
+      "IDGenerator-T9ST2": {}
+    };
+    let response = await langflowClient.runFlow(
+        flowIdOrName,
+        langflowId,
+        inputValue,
+        tweaks,
+        stream,
+        (data) => console.log("Received:", data.chunk), // onUpdate
+        (message) => console.log("Stream Closed:", message), // onClose
+        (error) => console.log("Stream Error:", error) // onError
+    );
+    if (!stream) {
+        const flowOutputs = response.outputs[0];
+        const firstComponentOutputs = flowOutputs.outputs[0];
+        const output = firstComponentOutputs.outputs.message;
+        // outputs[0].outputs[0].outputs.message.message.text
+        const message = output.message;
+        console.log("Final Output:", message.text);
+
+        // Send the response to the Interactive Avatar
+        if (!avatar.current) {
+          setDebug("Avatar API not initialized");
+          return;
+        }
+  
+        //send the Langflow response to the Interactive Avatar
+        await avatar.current
+          .speak({
+            text: message.text, taskType: TaskType.REPEAT, taskMode: TaskMode.SYNC
+          })
+          .catch((e) => {
+            setDebug(e.message);
+          });
+        setIsLoadingChat(false);
+        setInput("");          
+    }
+
+  } 
+
+  async function handleSubmitToFlowise() {
+
+    const flowIdOrName = process.env.NEXT_PUBLIC_FLOW_ID ?? "";
+    const inputValue = input;
+    const overrideConfig = { sessionId: chatSessionId };
+    let response = await flowiseClient.runFlow(
+        flowIdOrName,
+        inputValue,
+        overrideConfig,
+        (data) => console.log("Received:", data.chunk), // onUpdate
+        (message) => console.log("Stream Closed:", message), // onClose
+        (error) => console.log("Stream Error:", error) // onError
+    );
+    console.log("Final Output:", response.text);
+
+    // Send the response to the Interactive Avatar
+    if (!avatar.current) {
+      setDebug("Avatar API not initialized");
+      return;
+    }
+
+    //send the Flowise response to the Interactive Avatar
+    await avatar.current
+      .speak({
+        text: response.text, taskType: TaskType.REPEAT, taskMode: TaskMode.SYNC
+      })
+      .catch((e) => {
+        setDebug(e.message);
+      });
+    setIsLoadingChat(false);
+    setInput("");          
+
+  } 
+
 
   return (
     <div className="w-full flex flex-col gap-4">
       <Card>
-        <CardBody className="h-[500px] flex flex-col justify-center items-center">
+        <CardBody className="h-[700px] flex flex-col justify-start items-center pt-8 overflow-y-auto">
           {stream ? (
             <div className="h-[500px] w-[900px] justify-center items-center flex rounded-lg overflow-hidden">
               <video
@@ -231,6 +403,7 @@ export default function InteractiveAvatar() {
                 <Input
                   placeholder="Enter a custom knowledge ID"
                   value={knowledgeId}
+                  aria-label="Custom knowledge ID"
                   onChange={(e) => setKnowledgeId(e.target.value)}
                 />
                 <p className="text-sm font-medium leading-none">
@@ -239,11 +412,12 @@ export default function InteractiveAvatar() {
                 <Input
                   placeholder="Enter a custom avatar ID"
                   value={avatarId}
+                  aria-label="Custom avatar ID"
                   onChange={(e) => setAvatarId(e.target.value)}
                 />
                 <Select
-                  label="Select an avatar"
-                  aria-label="Select an avatar"
+                  label="Select avatar"
+                  aria-label="Select avatar"
                   placeholder="Or select one from these example avatars"
                   size="md"
                   onChange={(e) => {
@@ -258,6 +432,51 @@ export default function InteractiveAvatar() {
                       {avatar.name}
                     </SelectItem>
                   ))}
+                </Select>
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <p className="text-sm font-medium leading-none">
+                  Custom Voice ID (optional)
+                </p>
+                <Input
+                  value={voiceId}
+                  onChange={(e) => setVoiceId(e.target.value)}
+                  placeholder="Enter a custom voice ID"
+                  aria-label="Custom voice ID"
+                />
+                <Select
+                  label="Select voice"
+                  aria-label="Select voice"
+                  placeholder="Or select one from these example voices"
+                  size="md"
+                  onChange={(e) => {
+                    setVoiceId(e.target.value);
+                  }}
+                >
+                  {VOICES.map((voice) => (
+                    <SelectItem key={voice.voice_id} textValue={voice.voice_id}>
+                      {voice.name} | {voice.language} | {voice.gender}
+                    </SelectItem>
+                  ))}
+                </Select>
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <p className="text-sm font-medium leading-none">
+                  Quality
+                </p>
+                <Select
+                  label="Select quality"
+                  aria-label="Select quality"
+                  placeholder="Select quality"
+                  size="md"
+                  value={quality}
+                  onChange={(e) => {
+                    setQuality(e.target.value as AvatarQuality);
+                  }}
+                >
+                  <SelectItem value="low" key={"low"}>Low</SelectItem>
+                  <SelectItem value="medium" key={"medium"}>Medium</SelectItem>
+                  <SelectItem value="high" key={"high"}>High</SelectItem>
                 </Select>
                 <Select
                   label="Select language"
@@ -290,7 +509,7 @@ export default function InteractiveAvatar() {
           )}
         </CardBody>
         <Divider />
-        <CardFooter className="flex flex-col gap-3 relative">
+        <CardFooter className="flex flex-col gap-3">
           <Tabs
             aria-label="Options"
             selectedKey={chatMode}
@@ -302,15 +521,50 @@ export default function InteractiveAvatar() {
             <Tab key="voice_mode" title="Voice mode" />
           </Tabs>
           {chatMode === "text_mode" ? (
-            <div className="w-full flex relative">
+            <div className="w-full flex flex-col gap-4 relative">
               <InteractiveAvatarTextInput
                 disabled={!stream}
                 input={text}
-                label="Chat"
+                label="Repeat"
+                aria-label="Repeat"
                 loading={isLoadingRepeat}
                 placeholder="Type something for the avatar to respond"
                 setInput={setText}
                 onSubmit={handleSpeak}
+              />
+              <InteractiveAvatarTextInput
+                label="Chat-GPT"
+                aria-label="ChatGPT"
+                placeholder="Chat with the avatar (uses ChatGPT)"
+                input={input}
+                onSubmit={() => {
+                  setIsLoadingChat(true);
+                  if (!input) {
+                    setDebug("Please enter text to send to ChatGPT");
+                    return;
+                  }
+                  handleSubmit();
+                }}
+                setInput={setInput}
+                disabled={!stream}
+                loading={isLoadingChat}
+              />
+              <InteractiveAvatarTextInput
+                label="AI Agent"
+                aria-label="AI Agent"
+                placeholder="Chat with the AI Agent"
+                input={input}
+                onSubmit={() => {
+                  setIsLoadingChat(true);
+                  if (!input) {
+                    setDebug("Please enter a message to interact with the AI Agent");
+                    return;
+                  }
+                  handleSubmitToFlowise();
+                }}
+                setInput={setInput}
+                disabled={!stream}
+                loading={isLoadingChat}
               />
               {text && (
                 <Chip className="absolute right-16 top-3">Listening</Chip>
